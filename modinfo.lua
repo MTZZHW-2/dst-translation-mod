c27ad8991360bbd008333ba682d564286f2e0094
MTZZHW <34934749+MTZZHW@users.noreply.github.com>
2023-06-12 12:20:26 +0800

    first commit

---
+++ modinfo.lua
@@ -0,0 +1,21 @@
+name = "MTZZHW的汉化模组"
+description = [[
+  自用。
+
+  目前已汉化：
+  396026892 【大宝箱】Large Chest
+  2981932326【咖啡】   DST Coffee and More
+]]
+author = "MTZZHW"
+version = "0.7"
+forumthread = ""
+api_version = 10
+priority = -999999
+dst_compatible = true
+dont_starve_compatible = false
+reign_of_giants_compatible = false
+hamlet_compatible = false
+forge_compatible = false
+all_clients_require_mod = true 
+icon_atlas = ""
+icon = ""
---
+++ modmain.lua
@@ -0,0 +1,2 @@
+modimport("scripts/Large Chest.lua")
+modimport("scripts/DST Coffee and More.lua")
