36a6c178afd34782b5f2e6b632c7c0909a9e5496
MTZZHW <34934749+MTZZHW@users.noreply.github.com>
2023-06-15 21:22:16 +0800

    feat: 新增 Waiter 101 的汉化

--- modinfo.lua
+++ modinfo.lua
@@ -1,8 +1,8 @@
 name = "MTZZHW的汉化模组"
 author = "MTZZHW"
-version = "0.8.0"
+version = "0.9.2"
 description = [[
-  V0.8.0
+  V0.9.2
   
   自用。
 
@@ -10,6 +10,7 @@ description = [[
   396026892【大宝箱】Large Chest
   2981932326【咖啡】DST Coffee and More
   2859911011【农贸市场】Farmer's Market [Foodpack]
+  381565292【服务员 101】Waiter 101
 ]]
 
 dst_compatible = true
--- modmain.lua
+++ modmain.lua
@@ -1,3 +1,4 @@
 modimport("scripts/Large Chest.lua")
 modimport("scripts/DST Coffee and More.lua")
 modimport("scripts/Farmers Market Foodpack.lua")
+modimport("scripts/Waiter 101.lua")
